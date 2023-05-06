f4b50726be3ba83ef2b3efe08dd9dc9c44a3d627
Dariel Medina Rodríguez <79798431+darielmedr@users.noreply.github.com>
2023-05-06 13:54:35 +0200

    feat(advice-generator): add advice generator (#2)
    Add and integrate the advice-generator component.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AdviceGeneratorComponent } from './advice-generator/advice-generator.component';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AppComponent],
+      imports: [AdviceGeneratorComponent],
     }).compileComponents();
   });
 
@@ -12,5 +15,15 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
+  });
+
+  it('should render AdviceGeneratorComponent element', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+
+    const element = fixture.debugElement.query(
+      By.directive(AdviceGeneratorComponent),
+    );
+    expect(element).toBeTruthy();
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdviceGeneratorComponent } from './advice-generator/advice-generator.component';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [AdviceGeneratorComponent, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
