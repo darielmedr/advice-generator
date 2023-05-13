e2b73dc622c101206d8a32be2922942757c77faa
Jordan Bichot <jordan.bichot97@gmail.com>
2023-05-13 07:40:18 -0400

    feat: add advice-generator functionality and upgraded template

--- src/app/advice-generator/advice-generator.component.ts
+++ src/app/advice-generator/advice-generator.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AdviceHandlerService } from './data-access/advice-handler.service';
+import { Subject, takeUntil } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 @Component({
   standalone: true,
   selector: 'app-advice-generator',
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './advice-generator.component.html',
   styleUrls: ['./advice-generator.component.scss'],
 })
-export class AdviceGeneratorComponent {
+export class AdviceGeneratorComponent implements OnDestroy, OnInit {
+  private destroy = new Subject<void>();
+  public currentAdvice$ = this.adviceHandlerService.advice$;
+
+  constructor(private adviceHandlerService: AdviceHandlerService) {}
+
   public generateAdvice(): void {
-    console.log('generate new advice');
+    this.adviceHandlerService
+      .getNewAdvice()
+      .pipe(takeUntil(this.destroy))
+      .subscribe();
+  }
+
+  ngOnInit(): void {
+    this.generateAdvice();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
   }
 }
